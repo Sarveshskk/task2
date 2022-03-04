74da9f52746f896e8a93d475cd080e0353299cc6
Sarveshskk <sarvesh@excellencetechnologies.in>
2022-03-04 11:25:35 +0530

    first commit

---
+++ models/users.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+let usersSchema = mongoose.Schema({
+    first_name: {
+        type: String,
+        required: true
+    },
+    last_name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    username: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+});
+module.exports = mongoose.model('User', usersSchema);
